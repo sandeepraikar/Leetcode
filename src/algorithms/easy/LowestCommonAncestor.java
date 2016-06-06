5b99f954253a44965b68ccb2ff148a4d8a038d8f
sandeepraikar <snraikar@gmail.com>
2016-06-05 21:31:06 -0500

    Added solution for LCA in BST

---
+++ src/algorithms/easy/LowestCommonAncestor.java
@@ -0,0 +1,47 @@
+package algorithms.easy;
+
+
+/*
+ *  Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes v and w as the lowest node in T that has both v and w as descendants (where we allow a node to be a descendant of itself).”
+
+        _______6______
+       /              \
+    ___2__          ___8__
+   /      \        /      \
+   0      _4       7       9
+         /  \
+         3   5
+
+ * */
+/**
+ * @author Sandeep
+ *
+ */
+public class LowestCommonAncestor {
+
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+		if (root == null) {
+			return null;
+		}
+		if (p.val < root.val && q.val < root.val) {
+			return lowestCommonAncestor(root.left, p, q);
+		}
+		if (p.val > root.val && q.val > root.val) {
+			return lowestCommonAncestor(root.right, p, q);
+		} else {
+			return root;
+		}
+	}
+}
