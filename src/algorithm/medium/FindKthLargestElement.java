e703e23d182541ef377a7ca41994ceb84d0072e1
sandeepraikar <snraikar@gmail.com>
2016-06-07 19:56:51 -0500

    added solution for OddEvenLinkedList and KthLargest

--- src/algorithm/medium/FindKthLargestElement.java
+++ src/algorithm/medium/FindKthLargestElement.java
@@ -1,5 +1,6 @@
 package algorithm.medium;
 
+import java.util.Collections;
 import java.util.PriorityQueue;
 
 /*
@@ -16,16 +17,30 @@ public class FindKthLargestElement {
 
 	public static void main(String[] args) {
 		int[] arr ={3,2,1,5,6,4};
-		System.out.println(findKthLargest(arr, 2));
-
+		//System.out.println(findKthLargest(arr, 2));
+		PriorityQueue<Integer> queue = new PriorityQueue<>(3);
+		for(int i :arr){
+			queue.add(i);
+			if(queue.size()>2){
+				queue.poll();
+			}
+		}
+		queue.forEach(i-> System.out.print(i+" "));
 	}
 	public static int findKthLargest(int[] nums, int k) {
+		
         PriorityQueue<Integer> pq =new PriorityQueue<>(k+1);
         for(int x: nums){
         	pq.add(x);
+        	System.out.println("before:");
+        	pq.forEach(i-> System.out.print(i+" "));
         	if(pq.size()>k){
+        		
         		pq.poll();
+        		System.out.println("after");
+        		pq.forEach(i-> System.out.print(i+" "));
         	}
+        	System.out.println();
         }
         return pq.poll();
     }
---
+++ src/algorithm/medium/OddEvenLinkedList.java
@@ -0,0 +1,44 @@
+package algorithm.medium;
+
+/*328. Odd Even Linked List
+ * Given a singly linked list, group all odd nodes together followed by the even nodes. Please note here we are talking about the node number and not the value in the nodes.
+
+ You should try to do it in place. The program should run in O(1) space complexity and O(nodes) time complexity.
+
+ Example:
+ Given 1->2->3->4->5->NULL,
+ return 1->3->5->2->4->NULL. */
+/**
+ * @author Sandeep
+ *
+ */
+
+public class OddEvenLinkedList {
+
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+		}
+	}
+
+	public ListNode oddEvenList(ListNode head) {
+		if (head == null) {
+			return null;
+		}
+
+		ListNode odd = head;
+		ListNode even = head.next;
+		ListNode evenHead = even;
+		while (even != null && even.next != null) {
+			odd.next = odd.next.next;
+			even.next = even.next.next;
+			odd = odd.next;
+			even = even.next;
+		}
+		odd.next = evenHead;
+		return head;
+	}
+}
