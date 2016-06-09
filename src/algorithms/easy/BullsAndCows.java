a91201f8a0d43ece9a5df92d761d4385b7604c16
sandeepraikar <snraikar@gmail.com>
2016-06-08 19:00:35 -0500

    added solution for amazon interview question

--- src/algorithms/easy/BullsAndCows.java
+++ src/algorithms/easy/BullsAndCows.java
@@ -2,6 +2,7 @@ package algorithms.easy;
 
 
 /*
+ * 
  * You are playing the following Bulls and Cows game with your friend: You write down a number and ask your friend to guess what the number is. Each time your friend makes a guess, you provide a hint that indicates how many digits in said guess match your secret number exactly in both digit and position (called "bulls") and how many digits match the secret number but locate in the wrong position (called "cows"). Your friend will use successive guesses and hints to eventually derive the secret number.
 
 For example:
@@ -46,8 +47,7 @@ public class BullsAndCows {
 				 }else{
 					 if(numbers[secret.charAt(i)-'0']++<0) cowCount++;
 					 if(numbers[guess.charAt(i)-'0']-->0) cowCount++;
-				 }
-				 
+				 }				 
 			 }
 		 }else{
 			 return null;
