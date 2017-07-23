43f4f11ec00f1b6c0267bcc518c9c106f5c47e82
sandeepraikar <snraikar@gmail.com>
2017-07-22 19:43:29 -0700

    updated solution for hammingweight problem

--- src/algorithms/easy/NumberOf1Bits.java
+++ src/algorithms/easy/NumberOf1Bits.java
@@ -14,8 +14,19 @@ public class NumberOf1Bits {
 
 	public static void main(String[] args) {
 		System.out.println(hammingWeight(11));
+		System.out.println(hammingWeight2(11));
 	}
-
+	
+	// Efficient approach
+	public static int hammingWeight2(int n) {
+		int sum = 0;
+		while(n!=0){
+			sum++;
+			n = n&(n-1);
+		}
+		return sum;
+	}
+	
 	// you need to treat n as an unsigned value
 	public static int hammingWeight(int n) {
 		int count = 0;
