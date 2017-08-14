7a1486ac697ba4a6c36bffbf1574ca84d032a2ac
sandeepraikar <snraikar@gmail.com>
2017-08-14 00:30:04 -0700

    added solution for MaximumRectangularAreaHistogram

---
+++ src/algorithms/medium/MaximumRectangularAreaInHistogram.java
@@ -0,0 +1,45 @@
+package algorithms.medium;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class MaximumRectangularAreaInHistogram {
+
+	public static void main(String args[]){
+		int arr[] = {2,1,2};
+		System.out.println(findMaximumRectangularAreaInHistogram(arr));
+	}
+
+	private static int findMaximumRectangularAreaInHistogram(int[] input) {
+		Deque<Integer> stack = new LinkedList<Integer>();
+		int area=0, maxArea=0;
+		int i;
+		for(i=0;i<input.length;){
+			if(stack.isEmpty() || input[stack.peekFirst()]<=input[i]){
+				stack.offerFirst(i++);
+			}else{
+				int top = stack.pollFirst();
+				if(stack.isEmpty()){
+					area = input[top]*i;
+				}else{
+					area = input[top]*(i-stack.peekFirst()-1);
+				}
+				if(area>maxArea){
+					maxArea=area;
+				}
+			}
+		}
+		while(!stack.isEmpty()){
+			int top = stack.pollFirst();
+			if(stack.isEmpty()){
+				area = input[top]*i;
+			}else{
+				area = input[top]*(i-stack.peekFirst()-1);
+			}
+			if(area>maxArea){
+				maxArea=area;
+			}
+		}
+		return maxArea;
+	}
+}
